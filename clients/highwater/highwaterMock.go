8969de912f497a33fcffa5118f10cc3f8182d57e
Jamie <jamie@tidepool.org>
2014-09-19 16:22:28 +1200

    don't worry about params

--- clients/highwater/highwaterMock.go
+++ clients/highwater/highwaterMock.go
@@ -12,8 +12,8 @@ func NewMock() *HighwaterMockClient {
 
 func (client *HighwaterMockClient) PostServer(eventName, token string, params map[string]string) {
 
-	if eventName == "" || token == "" || len(params) <= 0 {
-		log.Panicf("missing required eventName[%s] token[%s] params[%v]", eventName, token, params)
+	if eventName == "" || token == "" {
+		log.Panicf("missing required eventName[%s] token[%s] params[%v]", eventName, token)
 	}
 
 	return
@@ -21,16 +21,16 @@ func (client *HighwaterMockClient) PostServer(eventName, token string, params ma
 
 func (client *HighwaterMockClient) PostThisUser(eventName, token string, params map[string]string) {
 
-	if eventName == "" || token == "" || len(params) <= 0 {
-		log.Panicf("missing required eventName[%s] token[%s] params[%v]", eventName, token, params)
+	if eventName == "" || token == "" {
+		log.Panicf("missing required eventName[%s] token[%s]", eventName, token)
 	}
 
 	return
 }
 
 func (client *HighwaterMockClient) PostWithUser(userId, eventName, token string, params map[string]string) {
-	if userId == "" || eventName == "" || token == "" || len(params) <= 0 {
-		log.Panicf("missing required userId[%s] eventName[%s] token[%s] params[%v]", userId, eventName, token, params)
+	if userId == "" || eventName == "" || token == "" {
+		log.Panicf("missing required userId[%s] eventName[%s] token[%s]", userId, eventName, token)
 	}
 
 	return

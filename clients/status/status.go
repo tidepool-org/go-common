0f84224e3745ecdf19ea625e3c73f57022852b91
Darin Krauss <darinkrauss@gmail.com>
2016-04-21 11:47:06 -0700

    Add new error code field to status; allows more useful error responses

--- clients/status/status.go
+++ clients/status/status.go
@@ -8,23 +8,32 @@ import (
 // Type status holds the status return from an http request.
 type Status struct {
 	Code   int    `json:"code"`
+	Error  *int   `json:"error,omitempty"`
 	Reason string `json:"reason"`
 }
 
 // NewStatus constructs a Status object; if no reason is provided, it uses the
 // standard one.
-func NewStatus(code int, reason string) Status {
-	s := Status{Code: code, Reason: reason}
+func NewStatus(statusCode int, reason string) Status {
+	s := Status{Code: statusCode, Reason: reason}
 	if s.Reason == "" {
-		s.Reason = http.StatusText(code)
+		s.Reason = http.StatusText(statusCode)
+	}
+	return s
+}
+
+func NewStatusWithError(statusCode int, errorCode int, reason string) Status {
+	s := Status{Code: statusCode, Error: &errorCode, Reason: reason}
+	if s.Reason == "" {
+		s.Reason = http.StatusText(statusCode)
 	}
 	return s
 }
 
 // NewStatus constructs a Status object; if no reason is provided, it uses the
 // standard one.
-func NewStatusf(code int, reason string, args ...interface{}) Status {
-	return Status{Code: code, Reason: fmt.Sprintf(reason, args...)}
+func NewStatusf(statusCode int, reason string, args ...interface{}) Status {
+	return Status{Code: statusCode, Reason: fmt.Sprintf(reason, args...)}
 }
 
 func StatusFromResponse(res *http.Response) Status {

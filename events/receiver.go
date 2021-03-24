3354417255c301d46ed48321f57f2fb31ce67135
Todd Kazakov <todd.kazakov@gmail.com>
2021-03-24 18:17:15 +0200

    Add shutdown to event consumer

--- events/receiver.go
+++ events/receiver.go
@@ -2,7 +2,6 @@ package events
 
 import (
 	"context"
-	"github.com/cloudevents/sdk-go/protocol/kafka_sarama/v2"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
 
@@ -13,52 +12,6 @@ type EventHandler interface {
 
 type EventConsumer interface {
 	RegisterHandler(handler EventHandler)
-	Start(ctx context.Context) error
-}
-
-var _ EventConsumer = &KafkaCloudEventsConsumer{}
-
-type KafkaCloudEventsConsumer struct {
-	client   cloudevents.Client
-	consumer *kafka_sarama.Consumer
-	handlers []EventHandler
-}
-
-func NewKafkaCloudEventsConsumer(config *CloudEventsConfig) (*KafkaCloudEventsConsumer, error) {
-	if err := validateConsumerConfig(config); err != nil {
-		return nil, err
-	}
-
-	consumer, err := kafka_sarama.NewConsumer(config.KafkaBrokers, config.SaramaConfig, config.KafkaConsumerGroup, config.GetPrefixedTopic())
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := cloudevents.NewClient(consumer, cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
-	if err != nil {
-		return nil, err
-	}
-
-	return &KafkaCloudEventsConsumer{
-		client:   client,
-		consumer: consumer,
-		handlers: make([]EventHandler, 0),
-	}, nil
-}
-
-func (k *KafkaCloudEventsConsumer) RegisterHandler(handler EventHandler) {
-	k.handlers = append(k.handlers, handler)
-}
-
-func (k *KafkaCloudEventsConsumer) Start(ctx context.Context) error {
-	defer k.consumer.Close(context.Background())
-	return k.client.StartReceiver(ctx, k.receive)
-}
-
-func (k *KafkaCloudEventsConsumer) receive(ce cloudevents.Event) {
-	for _, handler := range k.handlers {
-		if handler.CanHandle(ce) {
-			_ = handler.Handle(ce)
-		}
-	}
+	Start() error
+	Stop(ctx context.Context) error
 }
